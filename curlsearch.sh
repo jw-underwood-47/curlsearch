016f47a8aa96ef92a3c448cebb07e6416fd4de3e
jw-underwood-47 <jwunderwood47@gmail.com>
2026-07-21 20:42:09 -0400

    rename main script and bring working filter from example

---
+++ curlsearch.sh
@@ -0,0 +1,8 @@
+URL=$1
+FILE=$(date +%d%m%y)_$(echo "$URL" | sed 's|^http://||; s|^https://||; s|[/:?&=<>]|_|g').html
+
+# get the webpage and store it
+curl $URL > $FILE
+
+
+# search for keywords
--- example.sh
+++
@@ -1,8 +0,0 @@
-URL=$1
-FILE=$2_$(date +%d%m%y).html
-
-# get the webpage and store it
-curl $URL > $FILE
-
-
-# search for keywords
