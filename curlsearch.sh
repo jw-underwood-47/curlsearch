4ab75e1bd7fedb2d7a6a94a3d286577586b0efc0
jw-underwood-47 <jwunderwood47@gmail.com>
2026-07-22 19:51:18 -0400

    can now search as many terms as desired

--- curlsearch.sh
+++ curlsearch.sh
@@ -4,5 +4,15 @@ FILE=$(date +%d%m%y)_$(echo "$URL" | sed 's|^http://||; s|^https://||; s|[/:?&=<
 # get the webpage and store it
 curl $URL > $FILE
 
+# flush the input url from list of arguments
+shift
 
 # search for keywords
+for term in "$@"; do
+    printf " %s " "$term"
+    SEARCHFILE=temp_"$term".txt
+    grep "$term" "$FILE" > $SEARCHFILE
+    echo -n $(cat "$SEARCHFILE" | wc -l); echo " instances"
+    # optional cleanup to remove text files -- delete if not wanted
+    rm temp_"$term".txt
+done
