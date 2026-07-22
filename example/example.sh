ad7a12494259cac1f90f708802eb31d35999ccff
jw-underwood-47 <jwunderwood47@gmail.com>
2026-07-22 19:36:26 -0400

    flesh out example more

--- example/example.sh
+++ example/example.sh
@@ -1,8 +1,11 @@
 URL=https://sheriff.knoxcountytn.gov/ 
-FILE=$(date +%d%m%y)_$(echo "$URL" | sed 's|^http://||; s|^https://||; s|[/:?&=<>]|_|g').html
+FILE=$(date +%d%m%y%H)_$(echo "$URL" | sed 's|^http://||; s|^https://||; s|[/:?&=<>]|_|g').html
 
 # get the webpage and store it
 curl "$URL" > "$FILE"
 
 
 # search for keywords
+SEARCHFILE=holds.txt
+grep -i "HOLD" "$FILE" > $SEARCHFILE
+echo -n $(cat "$SEARCHFILE" | wc -l); echo " holds"
