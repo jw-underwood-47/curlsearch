6b287d0152732e486e1bad67ec4759c91512dfa7
jw-underwood-47 <jwunderwood47@gmail.com>
2026-07-22 20:18:05 -0400

    more portability and error checking with some AI help

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 For when you want to download a webpage and look for stuff  
   
 ## usage
-```./curlsearch.sh url term1 [term2] [etc] ```
-The first argument after the script is the url you want to download.
-Subsequent arguments are terms that you want to search for.
-As of right now, they are all case sensitive.  
-
+```./curlsearch.sh URL TERM1 [TERM2] [ETC] ```
+The first argument after the script is the url to download.
+Subsequent arguments are search terms.
+As of right now, they are all case sensitive.
+This can be changed by replacing `grep` with
+`grep -i` in line 16 of the script.  
+Program stores curled file as html, based on url and date.  
+  
 ## dependencies
+sh (in bash, zsh, and most other shells)  
+sed  
 curl  
-printf  
 grep  
 All of these should come with basically any terminal.
--- curlsearch.sh
+++ curlsearch.sh
@@ -1,10 +1,18 @@
 #! /bin/sh
 
-URL=$1
-FILE=$(date +%d%m%y)_$(echo "$URL" | sed 's|^http://||; s|^https://||; s|[/:?&=<>]|_|g').html
+# make sure the user enters at least two arguments
+if [ "$#" -lt 2 ]; then
+    printf 'Usage: %s URL TERM...\n' "$0" >&2
+    exit 1
+fi
 
-# get the webpage and store it
-curl "$URL" > "$FILE"
+URL=$1
+# generate name for stored HTML file
+DAY=$(date +%Y%m%d%H)
+FILE="$DAY"_$(printf "%s" "$URL" | sed 's|^http://||; s|^https://||; s|[/:?&=<>]|_|g').html
+
+# get the webpage and store it; exit if it fails
+curl -f "$URL" > "$FILE" || { printf "error downloading URL"; exit 1; }
 
 # flush the input url from list of arguments
 shift
@@ -13,7 +21,7 @@ shift
 for term in "$@"; do
     printf " %s " "$term"
     SEARCHFILE=temp_"$term".txt
-    grep "$term" "$FILE" > $SEARCHFILE
+    grep "$term" "$FILE" > "$SEARCHFILE"
     printf "%s instances\n" "$(wc -l < "$SEARCHFILE")"
     # optional cleanup to remove text files -- delete if not wanted
     rm "$SEARCHFILE"
