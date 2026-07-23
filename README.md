97285d74bfe60dab64ae26c62e7d7246e126688a
jw-underwood-47 <jwunderwood47@gmail.com>
2026-07-22 20:00:59 -0400

    improve portability, update README

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # curlsearch
-For when you want to download a webpage and look for stuff
+For when you want to download a webpage and look for stuff  
+  
+## usage
+```./curlsearch.sh url term1 [term2] [etc] ```
+The first argument after the script is the url you want to download.
+Subsequent arguments are terms that you want to search for.
+As of right now, they are all case sensitive.  
+
+## dependencies
+curl  
+printf  
+grep  
+All of these should come with basically any terminal.
--- curlsearch.sh
+++ curlsearch.sh
@@ -1,8 +1,10 @@
+#! /bin/sh
+
 URL=$1
 FILE=$(date +%d%m%y)_$(echo "$URL" | sed 's|^http://||; s|^https://||; s|[/:?&=<>]|_|g').html
 
 # get the webpage and store it
-curl $URL > $FILE
+curl "$URL" > "$FILE"
 
 # flush the input url from list of arguments
 shift
@@ -12,7 +14,7 @@ for term in "$@"; do
     printf " %s " "$term"
     SEARCHFILE=temp_"$term".txt
     grep "$term" "$FILE" > $SEARCHFILE
-    echo -n $(cat "$SEARCHFILE" | wc -l); echo " instances"
+    printf "%s instances\n" "$(wc -l < "$SEARCHFILE")"
     # optional cleanup to remove text files -- delete if not wanted
-    rm temp_"$term".txt
+    rm "$SEARCHFILE"
 done
